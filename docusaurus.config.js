8bf3ff41e24d677c9fba0597fb5d230ce53c85e1
linyuxuanlin <824676271@qq.com>
2020-12-13 16:13:07 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,7 +47,7 @@ module.exports = {
     hideableSidebar: true,
     navbar: {
       title: "Power's Wiki",
-      hideOnScroll: false,
+      hideOnScroll: true,
       //style: 'primary',
       /*
       logo: {
