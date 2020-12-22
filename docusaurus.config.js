8c94807132b9c225cd75ad7c27b01b5bd02f6873
linyuxuanlin <824676271@qq.com>
2020-12-22 11:17:47 +0800

    ...

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,11 +6,13 @@ module.exports = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "https://cos.ap-guangzhou.myqcloud.com/wiki-media-1253965369/doc/logo-white.svg",
+  favicon:
+    "https://cos.ap-guangzhou.myqcloud.com/wiki-media-1253965369/doc/logo-white.svg",
   //organizationName: "linyuxuanlin", // Usually your GitHub org/user name.
   //projectName: "Wiki_Docusaurus", // Usually your repo name.
   themeConfig: {
-    image: 'https://cos.ap-guangzhou.myqcloud.com/wiki-media-1253965369/doc/logo-white.svg',
+    image:
+      "https://cos.ap-guangzhou.myqcloud.com/wiki-media-1253965369/doc/logo-white.svg",
     algolia: {
       apiKey: "4cb7cbb290ebf1c47479ada3658c3d8e",
       indexName: "wiki",
@@ -40,7 +42,7 @@ module.exports = {
       switchConfig: {
         // Icon for the switch while in dark mode
         darkIcon: " ",
-        lightIcon: ' ',
+        lightIcon: " ",
 
         // CSS to apply to dark icon,
         // React inline style object
