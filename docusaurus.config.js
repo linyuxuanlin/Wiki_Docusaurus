f9f4fde21c3deb3dc56271fdc5527e4e5a4d0ba7
linyuxuanlin <824676271@qq.com>
2021-02-10 23:43:31 +0800

    Revert "test px"
    This reverts commit 442784d16149fc286877151efab731a3816c640d.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,7 +47,7 @@ module.exports = {
         // React inline style object
         // see https://reactjs.org/docs/dom-elements.html#style
         darkIconStyle: {
-          marginLeft: "5px",
+          marginLeft: "2px",
         },
 
         // Unicode icons such as '\u2600' will work
