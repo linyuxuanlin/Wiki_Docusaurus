12bddb26c4cdae9f378a50add8f40f72bd6a85c8
linyuxuanlin <824676271@qq.com>
2021-04-08 14:37:52 +0800

    up

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -132,7 +132,7 @@ module.exports = {
           feedOptions: {
             type: 'all', // required. 'rss' | 'feed' | 'all'
             title: 'Power\'s Blog', // default to siteConfig.title
-            description: '测试信息', // default to  `${siteConfig.title} Blog`
+            description: '个人博客', // default to  `${siteConfig.title} Blog`
             copyright: 'Copyright © ${new Date().getFullYear()} Power Lin',
             language: undefined, // possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
           },
