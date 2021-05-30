dd1a3e6ce952087cfefb7bebad37908c463c0c0e
linyuxuanlin <me@wiki-power.com>
2021-05-30 15:08:02 +0800

    up

--- sidebars.js
+++ sidebars.js
@@ -84,7 +84,7 @@ module.exports = {
         //'如何用Markdown写学术论文',
         '基于RSSHub搭建RSS生成器（群晖Docker）', // CSDN
         '基于Bitwarden搭建密码管理器（群晖Docker）', // CSDN
-        '基于acme.sh自动申请域名证书（群晖Docker）', // CSDN todo
+        '基于acme.sh自动申请域名证书（群晖Docker）', // CSDN
         '基于Calibre搭建在线书库（群晖Docker）', // CSDN todo
         '如何实现外网RDP远控（frp）', // CSDN todo
         '使用rdp访问群晖NAS', // CSDN todo
