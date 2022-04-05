a8930c87ea0aff7ea3b1a6bb4195b31669c92164
linyuxuanlin <824676271@qq.com>
2022-04-06 07:54:00 +0800

    更新 docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,7 +94,7 @@ const config = {
   ],
 
   stylesheets: [{
-    href: 'https://cdn.jsdelivr.net/npm/katex@0.15.3/dist/katex.min.css',
+    href: 'https://cdn.jsdelivr.net/npm/katex@0.15.2/dist/katex.min.css',
     type: 'text/css',
     integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
     crossorigin: 'anonymous',
