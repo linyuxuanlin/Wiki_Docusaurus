a5a7310625051559a586a75696eb2d9467800bb0
linyuxuanlin <824676271@qq.com>
2022-06-15 10:53:16 +0800

    Revert "更新 docusaurus.config.js"
    This reverts commit 0fe145216662d90b75cfb45fb8e223bf5cccb993.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -115,16 +115,16 @@ const config = {
         indexName: 'wiki-power',
 
         // Optional: see doc section below
-        //contextualSearch: true,
+        contextualSearch: true,
 
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         //externalUrlRegex: 'external\\.com|domain\\.com',
 
         // Optional: Algolia search parameters
-        //searchParameters: {},
+        searchParameters: {},
 
         // Optional: path for search page that enabled by default (`false` to disable it)
-        //searchPagePath: 'search',
+        searchPagePath: 'search',
 
         //... other Algolia params
       },
