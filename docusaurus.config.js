9bf4aee3deee698e3258a9874a98f2e93b430f4c
Power Lin <me@wiki-power.com>
2022-11-13 10:58:19 +0800

    test change algolia

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -113,11 +113,11 @@ const config = {
 
       algolia: {
 
-        apiKey: '5c07d8bf9c9928c4453857f6cad0420e',
+        apiKey: 'defe7fd8690822eed8e3c94801bab286',//5c07d8bf9c9928c4453857f6cad0420e
         indexName: 'wiki-power',
 
         // The application ID provided by Algolia
-        appId: 'BH4D9OD16A', //这个是默认的，文档见 https://autocomplete-experimental.netlify.app/docs/docsearchmodal/#appid，用自己的 ID IRO903CONI 反而搜不出内容
+        appId: 'IRO903CONI', //这个是默认的，文档见 https://autocomplete-experimental.netlify.app/docs/docsearchmodal/#appid，用自己的 ID IRO903CONI 反而搜不出内容 (BH4D9OD16A)
 
         // Public API key: it is safe to commit it
         // apiKey: 'defe7fd8690822eed8e3c94801bab286',
